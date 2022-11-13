3d8ed0aae2e4d415873c7f2a0ebb8d2ed31a1301
z <20373260@buaa.edu.cn>
2022-11-13 11:18:52 +0800

    model added

--- model/models.py
+++ model/models.py
@@ -42,15 +42,62 @@ class Student(User):
         ('博二', '博二'),
     )
 
-    grade = models.CharField(max_length=10, choices=grade_choice, default='大一')
+    grade = models.CharField(max_length=10, choices=grade_choice, default='大四')
 
     major = models.CharField(max_length=128)
 
 
 class Teacher(User):
+    profession_title = models.CharField(max_length=128)
+    # 职称
+    research_direction = models.CharField(max_length=128)
+    # 研究方向
+    lab_name = models.CharField(max_length=128)
 
 
 class SchoolMate(User):
+    school_name = models.CharField(max_length=128)
+
+    work_field = models.CharField(max_length=128)
+
+
+class Enterprise(models.Model):
+    name = models.CharField(max_length=128)
+
+    industry = models.CharField(max_length=128)
+    # 所属行业
+    place = models.CharField(max_length=128)
+    
+
+class Lab(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Post(models.Model):
+    content = models.CharField(max_length=1024)
+    last_update_time = models.DateTimeField()
+    last_update_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Resume(models.Model):
+    content = models.CharField(max_length=1024)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=128)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+    description = models.CharField(max_length=256)
+    demanding = models.CharField(max_length=256)
+    salary = models.IntegerField()
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=256)
 
 
 class Adminer(User):
+    # todo
+    pass
